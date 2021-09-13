b2fc43a56c6b057607fabadfbb5b5f0896c03077
Kevin Su <kevinsu@outlook.co.nz>
2021-09-13 16:01:12 +1200

    functionality fully complete plus css

--- src/App.js
+++ src/App.js
@@ -3,23 +3,47 @@ import './App.css';
 import React, {useState} from 'react';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 import _ from "lodash";
+import {v4 as uuidv4} from 'uuid';
+import background from './images/background.jpg';
 
 const item = {
-    id:"13012",
-    name: "nxxxx",
+    id:uuidv4(),
+    name: "Finish designs",
+    desc: "Finish all user page UX UI designs for the new up and coming Android App...",
+    to: "John",
+    from: "Mark",
+    group:"Design",
 }
 
 const item2 = {
-    id:"123",
-    name: "pepe",
+    id:uuidv4(),
+    name: "Code landing page",
+    desc: "Create landing page for the new website, ensure you follow the UI/UX of the designs!",
+    to: "James",
+    from: "Willhem",
+    group:"Frontend",
 }
 
 const item3 = {
-    id:"12324",
-    name: "pepe33",
+    id:uuidv4(),
+    name: "Code home page",
+    desc: "Finish programming homepage and all link/router interactions...",
+    to: "William",
+    from: "Mark",
+    group:"Frontend",
+}
+
+const item4 = {
+    id:uuidv4(),
+    name: "Code user interactions",
+    desc: "Finish all user interactions including changing user details, viewing a profile and following another user",
+    to: "John",
+    from: "Mark",
+    group:"Backend",
 }
 
 function App() {
+  const [name, setName] = useState();
   const [text, setText] = useState();
   const [assigned, setAssigned] = useState();
   const [assignedBy, setAssignedBy] = useState();
@@ -39,11 +63,11 @@ function App() {
         items:[]
     },
     "finished":{
-        title:"Finished",
-        items: []
+        title:"To Review",
+        items: [item4]
     },
     "questions":{
-        title:"Questions",
+        title:"Finished",
         items:[]
     }
   });
@@ -66,6 +90,13 @@ function App() {
   }
 
   const addItem = () => {
+    if(!text || text.trim().length === 0 ||
+       !name || name.trim().length === 0 ||
+       !assigned || assigned.trim().length === 0 ||
+       !assignedBy || assignedBy.trim().length === 0 ||
+       !group || group.trim().length === 0){
+        return
+    }
     setState(prev => {
         return {
             ...prev,
@@ -73,8 +104,12 @@ function App() {
                 title:"title",
                 items:[
                 {
-                    id:"1234",
-                    name: text
+                    id:uuidv4(),
+                    name: name,
+                    desc: text,
+                    to: assigned,
+                    from: assignedBy,
+                    group: group,
                 },
                 ...prev.todo.items]
             }
@@ -82,24 +117,33 @@ function App() {
     })
 
     setText("");
+    setName("");
+    setGroup("");
+    setAssigned("");
+    setAssignedBy("");
   }
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <div>
-          <input type="text" placeholder="Name..." value={text} onChange={(e) => setText(e.target.value)} />
-          <input type="text" placeholder="To..." value={text} onChange={(e) => setAssigned(e.target.value)} />
-          <input type="text" placeholder="From..." value={text} onChange={(e) => setAssignedBy(e.target.value)} />
-          <input type="text" placeholder="Group..." value={text} onChange={(e) => setGroup(e.target.value)} />
-          <button onClick={addItem}>Add</button>
-        </div>
-      <div className="App">
+    <div >
+      <img src = {background} style = {{width:"100%", height:"100vh"}}alt = "title" />
+
+      <div style={{position:"absolute", top:"0%", left:"0%", display:"flex", justifyContent:"space-between", width:"100%", padding:"8px"}}>
+          <h5 style={{color:"white"}}>Todo Lists</h5>
+          <div>
+              <input type="text" placeholder="Title" value={name} onChange={(e) => setName(e.target.value)} />
+              <input type="text" placeholder="Description" value={text} onChange={(e) => setText(e.target.value)} />
+              <input type="text" placeholder="To..." value={assigned} onChange={(e) => setAssigned(e.target.value)} />
+              <input type="text" placeholder="From..." value={assignedBy} onChange={(e) => setAssignedBy(e.target.value)} />
+              <input type="text" placeholder="Group..." value={group} onChange={(e) => setGroup(e.target.value)} />
+              <button className="btn btn-primary" style={{marginLeft:"10px"}} onClick={addItem}>Add</button>
+          </div>
+      </div>
+      <div className="App" style={{position:"absolute", top:"15%", left:"0%"}}>
       <DragDropContext onDragEnd={handleDragEnd}>
         {_.map(state, (data, key) => {
             return (
                 <div key = {key} className="column">
-                    <h3>{data.title}</h3>
+                    <h5>{data.title}</h5>
                     <Droppable droppableId={key}>
                         {(provided)=>{
                             return(
@@ -118,7 +162,15 @@ function App() {
                                                             {...provided.dragHandleProps}
                                                             className="item"
                                                         >
-                                                            <p>{x.name}</p>
+                                                            <div class="card">
+                                                              <h5 class="card-header"><p>{x.name}</p></h5>
+                                                              <h5 class="card-title badge badge-warning" style={{color:"white"}}>{x.group}</h5>
+                                                              <div class="card-body">
+                                                                <h5 class="card-title badge badge-primary">Assigned to {x.to}</h5>
+                                                                <h5 class="card-title badge badge-success"> by {x.from}</h5>
+                                                                <p class="card-text">{x.desc}</p>
+                                                              </div>
+                                                            </div>
                                                         </div>
 
                                                     )
